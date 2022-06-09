06ebd807de98c22bec240956d60cdeaea63b1c3f
sirawishW <sirawish.wo@ku.th>
2022-06-09 09:51:40 +0700

    Fix navbar bug

--- resources/views/layouts/nav-bar.blade.php
+++ resources/views/layouts/nav-bar.blade.php
@@ -2,11 +2,14 @@
   <div class="collapse" id="navbarToggleExternalContent">
     <div class="bg-dark p-4">
       <h4 class="text-white">Eatomizer</h4>
-      <span class="text-muted">Toggleable via the navbar brand.</span>
+      <span class="text-muted">Please Login if you want to add more cuisine</span>
+    </div>
+    <div class="bg-dark p-4">
+      <a href="" class="text-white">Login</a>
     </div>
   </div>
   <nav class="navbar navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
   </nav>
